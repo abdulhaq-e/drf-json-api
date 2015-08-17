0beedb6d13e9a6958fed22ed642585dda3883d3e
abdulhaq <el.ingeniero09@gmail.com>
2015-08-17 08:48:51 +0200

    fixed the json separator

--- tests/utils.py
+++ tests/utils.py
@@ -3,17 +3,31 @@ import json
 
 
 def dump_json(data):
-    import rest_framework
-
-    version = rest_framework.__version__.split(".")
 
     json_kwargs = {
         "sort_keys": True,
         "indent": 4,
     }
 
-    if version[0] >= "3":
-        json_kwargs["separators"] = (",", ": ")
+    import rest_framework
+
+    version = rest_framework.__version__.split(".")
+
+    if int(version[0]) <= 3 and int(version[1]) < 1:
+        from rest_framework.compat import (SHORT_SEPARATORS, LONG_SEPARATORS,
+                                           INDENT_SEPARATORS)
+        from rest_framework.settings import api_settings
+
+        compact = api_settings.COMPACT_JSON
+
+        if json_kwargs["indent"] is None:
+            separators = SHORT_SEPARATORS if compact else LONG_SEPARATORS
+        else:
+            separators = INDENT_SEPARATORS
+    else:
+        separators = (', ', ': ')
+
+    json_kwargs["separators"] = separators
 
     return force_bytes(json.dumps(data, **json_kwargs))
 
